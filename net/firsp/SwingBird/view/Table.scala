2319f9cfd8a4bab4112dfae06f492a7484ae4561
FirstSpring <firstspring1845@gmail.com>
2014-03-21 18:21:28 +0900

    はぁ

--- net/firsp/SwingBird/view/Table.scala
+++ net/firsp/SwingBird/view/Table.scala
@@ -1,6 +1,6 @@
 package net.firsp.SwingBird.view
 
-import javax.swing.{SwingUtilities, CellRendererPane, JScrollPane, JTable}
+import javax.swing._
 import javax.swing.table.AbstractTableModel
 import java.awt.{Container, Component, Dimension}
 import javax.swing.event.TableModelEvent
@@ -11,24 +11,43 @@ import twitter4j.Status
 import net.firsp.SwingBird.lib.UsefulFunc._
 import net.firsp.SwingBird.model.StatusModel
 import net.firsp.SwingBird.cache.Cache
+import java.awt.event.{KeyAdapter, ActionEvent, KeyEvent}
 
-class TimelineView extends JTable {
-	val model = new ListModel
+class TableList extends JTable {
 	setTableHeader(null)
 	setIntercellSpacing(new Dimension(0, 1))
 	setDefaultEditor(classOf[Object], null)
-	setModel(model)
 
-	override def tableChanged(e: TableModelEvent) = {
-		super.tableChanged(e)
-		if (getSelectedRow == 0) {
-			changeSelection(1, 0, false, false)
+	{
+		val im = getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_HOME, 0), "selectFirstRow")
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_END, 0), "selectLastRow")
+		im.put(KeyStroke.getKeyStroke('j'), "selectNextRow")
+		im.put(KeyStroke.getKeyStroke('k'), "selectPreviousRow")
+		def act[T](f: (ActionEvent) => T) = new AbstractAction("") {
+			override def actionPerformed(e: ActionEvent) = f(e)
 		}
-		val v = getScrollPane.getVerticalScrollBar
-		if (e.getFirstRow < getSelectedRow) {
-			this.prepareRenderer(getDefaultRenderer(classOf[Object]), e.getFirstRow, 0)
-			v.setValue(v.getValue + getRowHeight(e.getFirstRow))
-		}
+		val am = getActionMap
+		am.put("home", act((e) => {
+			getScrollPane.getVerticalScrollBar.setValue(0)
+			scrollRectToVisible(getCellRect(getSelectedRow, 0, false))
+		}))
+		im.put(KeyStroke.getKeyStroke('H'), "home")
+		am.put("end", act((e) => {
+			getScrollPane.getVerticalScrollBar.setValue(Integer.MAX_VALUE)
+			scrollRectToVisible(getCellRect(getSelectedRow, 0, false))
+		}))
+		im.put(KeyStroke.getKeyStroke('E'), "end")
+	}
+
+	def putAction[T](key: KeyStroke, action: (ActionEvent) => T) = {
+		val im = getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+		val am = getActionMap
+		val command = key.toString
+		im.put(key, command)
+		am.put(command, new AbstractAction(command) {
+			override def actionPerformed(e: ActionEvent) = action(e)
+		})
 	}
 
 	def getScrollPane = {
@@ -50,7 +69,46 @@ class TimelineView extends JTable {
 	}
 }
 
-class ListModel extends AbstractTableModel {
+class TimelineView extends TableList {
+	val model = new StatusListModel
+	setModel(model)
+
+	var lock = false
+
+	putAction(KeyStroke.getKeyStroke('s'), (e) => {
+		lock ^= true
+	})
+
+	addKeyListener(new KeyAdapter() {
+		override def keyTyped(e: KeyEvent) = {
+			val s = getValueAt(getSelectedRow, 0).asInstanceOf[StatusModel]
+			e.getKeyChar match {
+				case 'f' => //spawn(getTwitter.createFavorite(s.orgId))
+				case 'u' => //spawn(getTwitter.destroyFavorite(s.orgId))
+				case 'r' => //spawn(getTwitter.retweetStatus(s.orgId))
+				case _ =>
+			}
+		}
+	})
+
+	override def tableChanged(e: TableModelEvent) = {
+		super.tableChanged(e)
+		tryIgnore(this.prepareRenderer(getDefaultRenderer(classOf[Object]), e.getFirstRow, 0))
+		if (getSelectedRow == 0) {
+			changeSelection(1, 0, false, false)
+		}
+		val v = getScrollPane.getVerticalScrollBar
+		if (e.getFirstRow < getSelectedRow) {
+			if (lock) {
+				v.setValue(v.getValue + getRowHeight(e.getFirstRow))
+			}
+		}
+	}
+
+
+}
+
+class StatusListModel extends AbstractTableModel {
 
 	val arr = new ArrayBuffer[StatusModel]
 	val map = new HashSet[Long]
@@ -61,18 +119,18 @@ class ListModel extends AbstractTableModel {
 
 	def getValueAt(rowIndex: Int, columnIndex: Int): AnyRef = arr(rowIndex)
 
-	def insert(m: StatusModel):Unit = map.synchronized{
+	def insert(m: StatusModel): Unit = map.synchronized {
 		if (!map.contains(m.id)) {
 			arr.indexWhere(_.date.getTime <= m.date.getTime) match {
 				case -1 => {
 					val row = arr.size
-					invokeAndWait{
+					invokeAndWait {
 						arr += m
 						fireTableRowsInserted(row, row)
 					}
 				}
 				case row: Int => {
-					invokeAndWait{
+					invokeAndWait {
 						arr.insert(row, m)
 						fireTableRowsInserted(row, row)
 					}
@@ -82,5 +140,5 @@ class ListModel extends AbstractTableModel {
 		}
 	}
 
-	def insert(s: Status):Unit = insert(Cache.createModel(s))
+	def insert(s: Status): Unit = insert(Cache.createModel(s))
 }
